2541ab5503fe6c3f9631e49c540ab1314d2e6adb
Bevence <sitikhubikesh@gmail.com>
2021-03-02 11:48:28 +0545

    django-heroku

--- diabetes_prediction/settings.py
+++ diabetes_prediction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,4 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-LOGIN_REDIRECT_URL = '/'
+# LOGIN_REDIRECT_URL = '/'
+
+#Activate Django_Heroku
+django_heroku.settings(locals())
